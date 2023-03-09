d0de9c9c1f282532ddec51d0f0e6b917b607f128
Rick Arcanjo <ricardo.cassettari07@gmail.com>
2023-03-09 11:35:44 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Continue");
+Console.WriteLine(); 
+
+Console.WriteLine("Pressione uma tecla para continuar...");
+Console.ReadKey();
+Console.WriteLine();
+Console.WriteLine("Obrigado por abrir este Programa");
